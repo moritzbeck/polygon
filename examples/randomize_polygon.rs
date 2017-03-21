66d2b7a4e732a541836ee3be8e6f4445582310ef
moritzbeck <beck-moritz@t-online.de>
2017-03-21 03:17:15 +0100

    Improve generate module

--- examples/randomize_polygon.rs
+++ examples/randomize_polygon.rs
@@ -7,5 +7,5 @@ fn main() {
     let pts = vec![Point::new(0.0, 0.0); 10];
     let mut p = Polygon::from_points(&pts[..]);
     p.randomize(generate::Mode::QuickStarLike);
-    println!("Polygon: {:?}", p);
+    println!("Polygon: {:#?}", p);
 }
--- src/generate.rs
+++ src/generate.rs
@@ -14,8 +14,7 @@ pub enum Mode {
     TwoOptLike,
     /// Generates random points `p_1`, … ,`p_n`
     /// and chooses a point `q` in their convex hull.
-    /// Then sorts the vertices by the angle between the line
-    /// from `q` to `p_i` and the x-axis.
+    /// Then it orders the vertices by their circular order around `q`.
     /// The resulting polygon will be star-shaped.
     QuickStarLike
 }
@@ -29,6 +28,7 @@ impl Polygon {
         let mut rng = thread_rng();
         let range = Range::new(1.0, 500.0);
         let len = self.size();
+        assert!(len >= 3);
         {
             let points = self.points_mut();
             for i in 0..len {
@@ -39,105 +39,121 @@ impl Polygon {
         }
         match mode {
             Mode::TwoOptLike => {
-                let d = _two_opt_like(self);
+                let d = self._two_opt_like();
                 if d.is_none() {
                     self.randomize(mode); // try again
                     // TODO?: remove recursion
                 }
             },
             Mode::QuickStarLike => {
-                _quick_star_like(self);
+                self._quick_star_like();
             }
         }
         assert!(self.is_simple());
     }
-}
-/// Returns the number of swaps if it succeeds.
-fn _two_opt_like(p: &mut Polygon) -> Option<u32> {
-        // Ω(n^2)
-        let len = p.size();
-        assert!(len >= 3);
-        let points = p.points_mut();
-        let mut tangled = true;
-        let mut swaps = 0;
-        while tangled {
-            tangled = false;
-            let l1 = Line::new(points[len-1], points[0]);
-            for j in 1..len-2 {
-                let l2 = Line::new(points[j], points[j+1]);
-                if l1.intersects(&l2) {
-                    points.swap(0, j);
-                    swaps += 1;
-                    tangled = true;
-                    break;
-                }
-            }
-            for i in 0..len-3 {
-                for j in i+2..len-1 {
-                    let l1 = Line::new(points[i], points[i+1]);
+
+    /// Returns the number of swaps if it succeeds.
+    fn _two_opt_like(&mut self) -> Option<u32> {
+            // Ω(n^2)
+            let len = self.size();
+            let points = self.points_mut();
+            let mut tangled = true;
+            let mut swaps = 0;
+            while tangled {
+                tangled = false;
+                let l1 = Line::new(points[len-1], points[0]);
+                for j in 1..len-2 {
                     let l2 = Line::new(points[j], points[j+1]);
                     if l1.intersects(&l2) {
-                        points.swap(i+1, j);
+                        points.swap(0, j);
                         swaps += 1;
                         tangled = true;
                         break;
                     }
                 }
+                for i in 0..len-3 {
+                    for j in i+2..len-1 {
+                        let l1 = Line::new(points[i], points[i+1]);
+                        let l2 = Line::new(points[j], points[j+1]);
+                        if l1.intersects(&l2) {
+                            points.swap(i+1, j);
+                            swaps += 1;
+                            tangled = true;
+                            break;
+                        }
+                    }
+                }
+                if swaps >= 20_000 {
+                    // TODO: choose a good limit;
+                    // this limit should be dependent on the size of the polygon
+                    return None;
+                }
             }
-            if swaps >= 20_000 {
-                // TODO: choose a good limit;
-                // this limit should be dependent on the size of the polygon
-                return None;
+            Some(swaps)
+    }
+    /// Forms a star shaped polygon by reordering the given vertices.
+    ///
+    /// First it randomly selects 3 vertices and
+    /// computes a random point `p` in the induced triangle.
+    /// Then it orders the vertices by their circular order around `p`.
+    // TODO: This function could be useful even without first generating
+    //       random points (to get a random star with the same vertex set).
+    fn _quick_star_like(&mut self) {
+        use std::cmp::Ordering;
+
+        let mut rng = thread_rng();
+        let sample = sample(&mut rng, 0..self.size(), 3).into_iter()
+            .map(|s| { self.points()[s] })
+            .collect::<Vec<_>>();
+
+        // t0 + t1 + t2 == 1.0
+        let t0 = Range::new(0.0, 1.0).ind_sample(&mut rng);
+        let t1 = Range::new(0.0, 1.0 - t0).ind_sample(&mut rng);
+        let t2 = 1.0 - t0 - t1;
+        let p = sample[0] * t0 + sample[1] * t1 + sample[2] * t2;
+
+        // Sorts the points around p in CCW order.
+        // The minimal angle is the top direction.
+        // Ties are broken sometimes, by a strange method that is easy to implement.
+        self.points_mut().sort_by(|p1, p2| {
+            let p1 = *p1;
+            let p2 = *p2;
+            if p1 == p2 { return Ordering::Equal; }
+            if p1 == p  { return Ordering::Less; }
+            if p2 == p  { return Ordering::Greater; }
+            if p1.x < p.x && p2.x > p.x { return Ordering::Less; }
+            if p2.x < p.x && p1.x > p.x { return Ordering::Greater; }
+            match (Line::new(p, p1).slope(), Line::new(p, p2).slope()) {
+                (Slope::Vertical, Slope::Vertical) => {
+                    if p1.y > p2.y { Ordering::Less }
+                    else { Ordering::Greater }
+                },
+                (Slope::Vertical, _) => { //p1.x == p.x
+                    if p1.y > p.y { Ordering::Less }
+                    else { Ordering::Greater }
+                },
+                (_, Slope::Vertical) => {
+                    if p2.y > p.y { Ordering::Greater }
+                    else { Ordering::Less }
+                },
+                (Slope::Slope(m1), Slope::Slope(m2)) => {
+                    if m1 > m2 { Ordering::Greater }
+                    else { Ordering::Less }
+                }
             }
-        }
-        Some(swaps)
+        });
+        assert!(self.is_simple());
+    }
+    /// Returns a x-monotone simple polygon with the gicen vertices.
+    ///
+    /// First it sorts the vertices by x-coordinate.
+    /// Then it test for intersections of the 'closing edge' `p_np_1`
+    /// with another edge from left to right.
+    /// If an intersection is found the vertex with the smaller index
+    /// is pushed to the beginning of the backing `Vec`.
+    /// After one sweep the polygon will be simple.
+    fn _x_monotone(&mut self) {
+        #![allow(unused_variables)]
+        unimplemented!();
+    }
 }
-/// Returns a star shaped polygon with the given vertices.
-///
-/// First it randomly selects 3 vertices and
-/// computes a random point `p` in the induced triangle.
-/// Then it orders the vertices by their circular order around `p`.
-fn _quick_star_like(pol: &mut Polygon) {
-    use std::cmp::Ordering;
-
-    let mut rng = thread_rng();
-    let sample = sample(&mut rng, 0..pol.size(), 3).into_iter()
-        .map(|s| { pol.points()[s] })
-        .collect::<Vec<_>>();
-
-    // t0 + t1 + t2 == 1.0
-    let t0 = Range::new(0.0, 1.0).ind_sample(&mut rng);
-    let t1 = Range::new(0.0, 1.0 - t0).ind_sample(&mut rng);
-    let t2 = 1.0 - t0 - t1;
-    let p = sample[0] * t0 + sample[1] * t1 + sample[2] * t2;
-
-    // Sorts the points around p in CCW order.
-    // The minimal angle is the top direction.
-    // Ties are broken sometimes, by a strange method that is easy to implement.
-    pol.points_mut().sort_by(|p1, p2| {
-        let p1 = *p1;
-        let p2 = *p2;
-        if p1 == p2 { return Ordering::Equal; }
-        if p1 == p  { return Ordering::Less; }
-        if p2 == p  { return Ordering::Greater; }
-        if p1.x < p.x && p2.x > p.x { return Ordering::Less; }
-        if p2.x < p.x && p1.x > p.x { return Ordering::Greater; }
-        match (Line::new(p, p1).slope(), Line::new(p, p2).slope()) {
-            (Slope::Vertical, Slope::Vertical) => {
-                p2.y.partial_cmp(&p1.y).expect("y coordinate should never be NaN!")
-            },
-            (Slope::Vertical, _) => { //p1.x == p.x
-                if p1.y > p.y { Ordering::Less }
-                else { Ordering::Greater }
-            },
-            (_, Slope::Vertical) => {
-                if p2.y > p.y { Ordering::Greater }
-                else { Ordering::Less }
-            },
-            (Slope::Slope(m1), Slope::Slope(m2)) => {
-                m1.partial_cmp(&m2).expect("Slope should never be NaN!")
-            }
-        }
-    });
-}
-
